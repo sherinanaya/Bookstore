77cbe0a214ca360da448c9fc4d9fa01a67a1ef40
sherinanaya <anaya.sherin@gmail.com>
2025-01-13 19:06:54 +0530

    Update BookstoreApplication.java

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -3,6 +3,7 @@ package com.example.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* Annotation */
 @SpringBootApplication
 public class BookstoreApplication {
 
