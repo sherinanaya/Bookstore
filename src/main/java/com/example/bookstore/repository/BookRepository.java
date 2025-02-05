1f67fea2c1499a89d6c9bd01bf693171645bc5f4
sherinanaya <anaya.sherin@gmail.com>
2025-02-05 12:29:31 +0530

    Update BookRepository.java

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -1,13 +1,19 @@
 package com.example.bookstore.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.example.bookstore.entity.Book;
 
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+	@Query(value = "SELECT * FROM books "
+			+ "WHERE CONCAT(books.name, ' ', books.author, ' ', books.price, ' ', books.quantity)"
+			+ "LIKE %?1%", nativeQuery = true)
+	public List<Book> findByKeyword(String keyword);
 }
 
