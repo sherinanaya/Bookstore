d8367e6aef42ddacd385cb1ef54b94ee863a9d70
sherinanaya <anaya.sherin@gmail.com>
2025-02-19 22:18:51 +0530

    Update BookService.java

--- src/main/java/com/example/bookstore/service/BookService.java
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.bookstore.entity.Book;
 import com.example.bookstore.repository.BookRepository;
+//import com.example.bookstore.repository.CartRepository;
+//import com.example.bookstore.repository.CartItemRepository;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -14,6 +16,12 @@ public class BookService {
 	
 	@Autowired
 	private BookRepository bookRepository;
+	
+	//@Autowired
+	//private CartRepository cartRepository;
+
+	//@Autowired
+	//private CartItemRepository cartItemRepository;
 
 	public BookService(BookRepository bookRepository) {
 		this.bookRepository = bookRepository;
@@ -47,5 +55,44 @@ public class BookService {
 		else
 			return (List<Book>) bookRepository.findAll();
 	}
-	
+	/*
+	public Cart getCart(Long cartId) {
+		return cartRepository.findById(cartId).orElseGet(() -> {
+			Cart newCart = new Cart();
+			return cartRepository.save(newCart);
+		});
+	}
+
+	public Cart addToCart(Long cartId, Long bookId) {
+		Cart cart = getCart(cartId);
+
+		Optional<Book> bookOptional = bookRepository.findById(bookId);
+		if (!bookOptional.isPresent()) {
+			throw new RuntimeException("Book not found");
+		}
+
+		Book book = bookOptional.get();
+
+		Optional<CartItem> existingCartItem = cart.getCartItems().stream()
+				.filter(item -> item.getBook().getId().equals(bookId))
+				.findFirst();
+
+		if (existingCartItem.isPresent()) {
+			existingCartItem.get().setQuantity(existingCartItem.get().getQuantity() + 1);
+		} else {
+			CartItem cartItem = new CartItem();
+			cartItem.setBook(book);
+			cartItem.setQuantity(1);
+			cartItem.setCart(cart);
+			cart.getCartItems().add(cartItem);
+		}
+
+		cart.setTotalPrice(cart.getCartItems().stream().mapToDouble(item -> item.getBook().getPrice() * item.getQuantity()).sum());
+		return cartRepository.save(cart);
+	}
+
+	public List<CartItem> getCartItems(Long cartId) {
+		Cart cart = getCart(cartId);
+		return cart.getCartItems().stream().filter(item -> item.getQuantity() > 0).toList();
+	}*/
 }
