9c789232c1dc2160084c6ba2bb058d34101577f0
sherinanaya <anaya.sherin@gmail.com>
2025-02-25 23:51:15 +0530

    Create CartItem.java

---
+++ src/main/java/com/example/bookstore/entity/CartItem.java
@@ -0,0 +1,45 @@
+package com.example.bookstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private int quantity;
+
+    public CartItem() {}
+
+    public CartItem(Book book, int quantity) {
+        this.book = book;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
