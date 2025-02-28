dee7960e1ed5d5b68b7de4905f982b7c19753d89
sherinanaya <anaya.sherin@gmail.com>
2025-02-28 23:10:57 +0530

    Update BookRepository.java

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.bookstore.entity.Book;
-import com.example.bookstore.entity.CartItem;
-import com.example.bookstore.entity.Cart;
+//import com.example.bookstore.entity.CartItem;
+//import com.example.bookstore.entity.Cart;
 
 
 
